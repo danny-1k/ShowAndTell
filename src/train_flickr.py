69ac88f622dd9bdd9f19bb55906b42264853baea
Daniel Ik <codingeinstein@gmail.com>
2022-12-04 18:25:38 +0100

    train on flickr

---
+++ src/train_flickr.py
@@ -0,0 +1,133 @@
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+
+    parser = ArgumentParser()
+    
+    parser.add_argument('--lr', default=1e-4, type=float)
+    parser.add_argument('--encoder', default='vgg16')
+    parser.add_argument('--epochs', default=100, type=int, help='Maximum number of epochs to train for')
+    parser.add_argument('--seq_len', default=100, type=int, help="Maximum number of tokens per sequence. Truncation may occur for some sequences in the training dataset")
+    parser.add_argument('--save_dir', required=True, help="Where should I save the checkpoints?")
+    parser.add_argument('--resume', action='store_true')
+    parser.add_argument('--checkpoint_name', default='best_checkpoint.pth')
+
+
+    args = parser.parse_args()
+
+    import os
+    import torch
+    import torch.nn as nn
+    from tokenizer import Tokenizer
+    from torch.optim import Adam, SGD
+    from torch.utils.data import DataLoader
+    from torchvision.models.vgg import vgg19, vgg16
+    from data import Flickr
+    from models import ShowAndTellNet
+    from tqdm import tqdm
+    from utils import save_checkpoint, load_checkpoint
+
+
+    tokenizer = Tokenizer('../tokens', max_len=args)
+    tokenizer.load()
+
+    vocab_size = len(tokenizer.tokens)
+    encoder = {'vgg16': vgg16, 'vgg19': vgg19}[args.encoder](pretrained=True).eval().cuda()
+
+    for param in encoder.parameters():
+        param.requires_grad = False
+
+    net = ShowAndTellNet(vocab_size=vocab_size, encoder=encoder).cuda()
+
+    epochs = args.epochs
+    lossfn = nn.CrossEntropyLoss()
+    optimizer = SGD(net.parameters(), lr=args.lr)
+    
+    train = DataLoader(
+        Flickr(train=True),
+        batch_size=64,
+        shuffle=True,
+    )
+
+    test = DataLoader(
+        Flickr(train=False),
+        batch_size=64,
+        shuffle=True,
+    )
+
+    lowest_loss = float('inf')
+
+    train_loss = 0
+    test_loss = 0
+
+    if args.resume:
+        if os.path.exists(os.path.join(args.save_dir, args.checkpoint_name)):
+            checkpoint = load_checkpoint(os.path.join(args.save_dir, args.checkpoint_name))
+            print(f'Loaded Checkpoint at Epoch {checkpoint["epoch"]} & Best Loss {checkpoint["best_loss"]:.3f}')
+            net.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            train_loss = checkpoint['train_loss']
+            test_loss = checkpoint['test_loss']
+            lowest_loss = checkpoint['best_loss']
+            start_epoch = checkpoint['epoch']-1
+        else:
+            start_epoch = 0
+            print('Failed to load checkpoint... Training from scratch')
+
+    for epoch in range(start_epoch, epochs):
+        net.train()
+        for x,y in tqdm(train):
+            x = x.cuda()
+            y = y.cuda()
+
+            optimizer.zero_grad()
+            p = net(x, max_len=args.seq_len) # (N, seq_len, 7689), Y -> (N, seq_len)
+
+            loss = lossfn(
+                p.view(p.shape[0]*p.shape[1], -1),
+                y.long().view(-1)
+            )
+
+
+            train_loss = (1-.7)*train_loss + .7*loss.item()
+
+            loss.backward()
+            optimizer.step()
+
+
+        with torch.no_grad():
+            net.eval()
+
+            for x,y in tqdm(test):
+                x = x.cuda()
+                y = y.cuda()
+
+                p = net(x, max_len=args.seq_len) # (N, seq_len, 7689), Y -> (N, seq_len)
+
+                loss = lossfn(
+                    p.view(p.shape[0]*p.shape[1], -1),
+                    y.long().view(-1)
+                )
+
+
+                test_loss = (1-.7)*test_loss + .7*loss.item()
+
+
+        if test_loss < lowest_loss:
+            lowest_loss = test_loss
+
+        checkpoint = {
+            'epoch': epoch+1,
+            'train_loss': train_loss,
+            'test_loss': test_loss,
+            'best_loss': lowest_loss,
+            'state_dict': net.state_dict(),
+            'optimizer': optimizer.state_dict(),
+        }
+
+        if test_loss == lowest_loss:
+            save_checkpoint(checkpoint, os.path.join(args.save_dir, 'best_checkpoint.pth'))
+        else:
+            save_checkpoint(checkpoint,  os.path.join(args.save_dir, 'checkpoint.pth'))
+            
+        print(f'EPOCH : {epoch+1} Train Loss : {train_loss:.3f} Test Loss: {test_loss:3f}')
