3ffb031718b8cc2678da6911802149622c20a54a
Daniel Ik <codingeinstein@gmail.com>
2022-12-05 01:09:03 +0100

    remove nans in fix_flicker_dataset.py

--- src/fix_flickr_dataset.py
+++ src/fix_flickr_dataset.py
@@ -6,3 +6,10 @@
 f = open('../data/flickr30k/results.csv', 'r').read()
 f = f.replace('| ', '|')
 open('../data/flickr30k/results.csv', 'w').write(f)
+
+import os
+import pandas as pd
+
+df = pd.read_csv(os.path.join('../data/flickr30k/results.csv'), sep='|')
+df.dropna(axis=0, inplace=True)
+df.to_csv('../data/flickr30k/results.csv') # this will save the tsv as a csv (delimeter -> ",")
--- src/save_flickr_tokens.py
+++ src/save_flickr_tokens.py
@@ -7,13 +7,9 @@ tokenizer = Tokenizer('../tokens/flickr')
 
 df = pd.read_csv(os.path.join('../data/flickr30k/results.csv'), sep='|')
 
-df.dropna(axis=0, inplace=True)
-
 comments = df['comment']
 
 for idx in tqdm(range(len(comments))):
   tokenizer.update_tokens(comments.iloc[idx])
 
 tokenizer.save()
-
-df.to_csv('../data/flickr30k/results.csv') # this will save the tsv as a csv (delimeter -> ",")
