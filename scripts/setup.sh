3641edb6191a625df3d7dcc69dced7b031fd1a93
Daniel Ik <codingeinstein@gmail.com>
2023-03-24 07:53:18 +0000

    Setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,16 @@
+# Create Needed folders
+
+if [ ! -d "../runs" ]
+then 
+    mkdir ../runs
+fi
+
+if [ ! -d "../data" ]
+then
+    mkdir "../data"
+fi
+
+if [ ! -d "../checkpoints" ]
+then
+    mkdir "../checkpoints"
+fi
